26ad6ab6a4f53c817263023931e3df5dfe6c2e66
HaraLambda3 <hara.heique@lambda3.com.br>
2022-12-24 16:11:13 -0300

    feat: adição da configuração de injeção de dependencias

---
+++ apps/api/src/BeautyControl.API/Configurations/DependecyInjectionConfig.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System.Reflection;
+
+namespace BeautyControl.API.Configurations
+{
+    public static class DependecyInjectionConfig
+    {
+        public static void RegisterDependecies(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            builder.Services.AddHttpContextAccessor();
+        }
+    }
+}
