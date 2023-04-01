75c1cecef669e8c22a1fe0d24e65cd38a9336dbf
HaraHeique <heikacademicos@hotmail.com>
2023-04-01 15:44:20 -0300

    feat: extension utilitária para obter o tipo baseado no filter attribute DisplayName

---
+++ apps/api/src/BeautyControl.API/Extensions/DeclarionTypeExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace BeautyControl.API.Extensions
+{
+    public static class DeclarionTypeExtensions
+    {
+        public static string GetDisplayName(this Type type, bool inherit = false)
+        {
+            return type.GetCustomAttributes(inherit)
+                .OfType<DisplayNameAttribute>()
+                .FirstOrDefault()?.DisplayName ?? type.Name;
+        }
+    }
+}
