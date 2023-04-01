4c5b670d78235332115975466d4b0f9a3a40f1db
HaraHeique <heikacademicos@hotmail.com>
2023-04-01 15:41:31 -0300

    feat: criação dos pipeline behaviors de logging e validação das requests/commands

---
+++ apps/api/src/BeautyControl.API/Features/Common/PipelineBehaviors/LoggingBehavior.cs
@@ -0,0 +1,28 @@
+﻿using BeautyControl.API.Extensions;
+using MediatR;
+
+namespace BeautyControl.API.Features.Common.PipelineBehaviors
+{
+    public class LoggingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>
+    {
+        private readonly ILogger<LoggingBehavior<TRequest, TResponse>> _logger;
+
+        public LoggingBehavior(ILogger<LoggingBehavior<TRequest, TResponse>> logger) 
+            => _logger = logger;
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            var requestName = request.GetType().GetDisplayName();
+
+            _logger.LogInformation("Início do processamento da request {RequestName}: {@Request}", requestName, request);
+
+            var response = await next();
+            var responseName = response?.GetType().GetDisplayName();
+
+            _logger.LogInformation("Finalização do processamento da request {RequestName} com response {ResponseName}: {@Response}", requestName, responseName, response);
+
+            return response;
+        }
+    }
+}
---
+++ apps/api/src/BeautyControl.API/Features/Common/PipelineBehaviors/RequestValidationBehavior.cs
@@ -0,0 +1,89 @@
+﻿using BeautyControl.API.Extensions;
+using FluentResults;
+using FluentValidation;
+using FluentValidation.Results;
+using MediatR;
+
+#nullable disable
+namespace BeautyControl.API.Features.Common.PipelineBehaviors
+{
+    public abstract class RequestValidationBehavior<TRequest>
+    {
+        protected readonly IValidator<TRequest> Validator;
+        protected readonly ILogger<RequestValidationBehavior<TRequest>> Logger;
+
+        protected RequestValidationBehavior(
+            IValidator<TRequest> validator, 
+            ILogger<RequestValidationBehavior<TRequest>> logger)
+        {
+            Validator = validator;
+            Logger = logger;
+        }
+
+        protected async Task<ValidationResult> ValidateRequest(TRequest request, CancellationToken cancellationToken)
+        {
+            var validationResult = await Validator.ValidateAsync(request, cancellationToken);
+
+            if (validationResult.IsValid == false)
+            {
+                var requestName = typeof(TRequest).GetDisplayName();
+                var errors = validationResult.Errors;
+
+                var message = string.Join(
+                    Environment.NewLine,
+                    "Mensagem: Foram obtidos erros ao validar a request {RequestName}",
+                    "Erros: {@Errors}",
+                    "Payload: {@Payload}"
+                );
+
+                Logger.LogError(message, requestName, errors, request);
+            }
+
+            return validationResult;
+        }
+    }
+
+    public sealed class FluentResultRequestValidationBehavior<TRequest, TResponse> 
+        : RequestValidationBehavior<TRequest>, IPipelineBehavior<TRequest, TResponse>
+            where TRequest : IRequest<TResponse>
+            where TResponse : ResultBase
+    {
+        public FluentResultRequestValidationBehavior(
+            IValidator<TRequest> validator, 
+            ILogger<FluentResultRequestValidationBehavior<TRequest, TResponse>> logger) 
+            : base(validator, logger) { }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            var validationResult = await ValidateRequest(request, cancellationToken);
+
+            if (validationResult.IsValid) 
+                return await next();
+            
+            var errors = validationResult.Errors.Select(vf => vf.ErrorMessage);
+
+            return Result.Fail(errors) as dynamic;
+        }
+    }
+    
+    public sealed class FluentValidationRequestValidationBehavior<TRequest, TResponse>
+        : RequestValidationBehavior<TRequest>, IPipelineBehavior<TRequest, TResponse>
+            where TRequest : IRequest<TResponse>
+            where TResponse : ValidationResult
+    {
+        public FluentValidationRequestValidationBehavior(
+            IValidator<TRequest> validator,
+            ILogger<FluentValidationRequestValidationBehavior<TRequest, TResponse>> logger)
+            : base(validator, logger) { }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            var validationResult = await ValidateRequest(request, cancellationToken);
+
+            if (validationResult.IsValid) 
+                return await next();
+
+            return validationResult as dynamic;
+        }
+    }
+}
