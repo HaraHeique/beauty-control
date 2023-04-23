dfb8d88ca07eb6f7a343d2759f6f9c126da6dfd8
HaraHeique <heikacademicos@hotmail.com>
2023-04-23 15:26:03 -0300

    feat: adição do tratamento de exceção de domínio de forma global (por enquanto só retorna a mensagem)

--- apps/api/src/BeautyControl.API/Middlewares/ExceptionMiddleware.cs
+++ apps/api/src/BeautyControl.API/Middlewares/ExceptionMiddleware.cs
@@ -1,4 +1,6 @@
 ﻿#nullable disable
+using BeautyControl.API.Domain._Common.Exceptions;
+
 namespace BeautyControl.API.Middlewares
 {
     public class ExceptionMiddleware
@@ -18,17 +20,35 @@ namespace BeautyControl.API.Middlewares
             {
                 await _next(httpContext);
             }
+            catch (DomainException ex)
+            {
+                await HandleDomainException(httpContext, ex);
+            }
             catch (Exception ex)
             {
-                await HandleGenericExceptionAsync(httpContext, ex);
+                await HandleGenericException(httpContext, ex);
             }
         }
 
-        private async Task HandleGenericExceptionAsync(HttpContext context, Exception exception)
+        private async Task HandleGenericException(HttpContext context, Exception exception)
         {
             _logger.LogError(exception, "Um erro genérico ocorreu");
+
             context.Response.StatusCode = StatusCodes.Status500InternalServerError;
             await context.Response.WriteAsJsonAsync("Ocorreu um erro inesperado. Favor contate ao time de suporte.");
+        }
+
+        private async Task HandleDomainException(HttpContext context, DomainException exception)
+        {
+            _logger.LogError(exception, exception.Message);
+
+            context.Response.StatusCode = StatusCodes.Status422UnprocessableEntity;
+
+            var errorMessage = string.IsNullOrWhiteSpace(exception.Message) ? 
+                "Ocorreu um erro de negócio que não pode ser processado." : 
+                exception.Message;
+
+            await context.Response.WriteAsJsonAsync(errorMessage);
         }
     }
 }
