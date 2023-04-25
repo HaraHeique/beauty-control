72a13641b1c9d478279ed0ba475262f8665d558c
HaraHeique <heikacademicos@hotmail.com>
2023-04-24 22:01:12 -0300

    feat: criação dos métodos de extensão para HttpStatusCode

---
+++ apps/api/src/BeautyControl.API/Extensions/HttpStatusCodeExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+
+namespace BeautyControl.API.Extensions
+{
+    public static class HttpStatusCodeExtensions
+    {
+        public static int ToInt(this HttpStatusCode statusCode) => (int)statusCode;
+
+        public static bool IsSuccessful(this HttpStatusCode statusCode)
+        {
+            var castedValue = statusCode.ToInt();
+
+            return castedValue >= 200 && castedValue <= 299;
+        }
+
+        public static bool IsFailed(this HttpStatusCode statusCode)
+            => statusCode.IsSuccessful() == false;
+    }
+}
