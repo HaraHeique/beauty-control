245441f44dbb3b8b338ef1eb631cb780050c3737
HaraHeique <heikacademicos@hotmail.com>
2023-04-24 22:00:19 -0300

    feat: adição da implementação Enumeration class base e da classe de UserRoles

---
+++ apps/api/src/BeautyControl.API/Infra/Identity/Models/UserRoles.cs
@@ -0,0 +1,15 @@
+﻿using BeautyControl.API.Domain._Common;
+
+namespace BeautyControl.API.Infra.Identity.Models
+{
+    public class UserRoles : Enumeration
+    {
+        public const string AdminDisplayName = "Admin";
+        public const string EmployeeDisplayName = "Employee";
+
+        public static readonly UserRoles Admin = new(0, AdminDisplayName);
+        public static readonly UserRoles Employee = new(1, EmployeeDisplayName);
+
+        private UserRoles(int value, string name) : base(value, name) { }
+    }
+}
